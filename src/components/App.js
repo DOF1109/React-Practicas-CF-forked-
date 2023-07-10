73cb193f9ab20cc4dfe4754bd2c4cdee7d093f94
DOF1109 <friasdaniel.1109@gmail.com>
2023-07-10 11:42:29 -0300

    Creo el componente PresentationCard

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import PresentationCard from "./PresentationCard";
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="App">
+            <PresentationCard/>
+        </div>
+    );
+}
---
+++ src/components/PresentationCard.jsx
@@ -0,0 +1,17 @@
+import fotoPerfil from '../assets/foto-perfil.jfif';
+import './PresentationCard.css';
+
+ function PresentationCard() {
+    let nombre = 'Daniel';
+
+    return (
+        <div className="presentation-card">
+            <img className='foto-perfil' src={fotoPerfil} alt="Foto de perfil" />
+            <h1>
+                Hola, soy {nombre} estoy probando React
+            </h1>
+        </div>
+    );
+}
+
+export default PresentationCard;
